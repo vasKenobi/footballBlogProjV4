5d6f07b36dd6561f59bd4aaa7eb8d4275a16ddea
Vasyl Kle <vasyl.klets@gmail.com>
2019-12-06 19:24:01 +0200

    Popular Categories updated

--- src/App/Main/Widgets/Popularcategories.js
+++ src/App/Main/Widgets/Popularcategories.js
@@ -8,9 +8,9 @@ const Popularcategories = () => {
                                 <h2 className="widget-title">Popular Categories</h2>
                                 <div className="link-widget">
                                     <ul>
-                                        <li><Link to="/EnglishPL">English Premier League <span>(888)</span></Link></li>
-                                        <li><Link to="/MajorSL">Major Soccer League <span>(999)</span></Link></li>
-                                        <li><Link to="/GermanBL">German Bundesliga <span>(444)</span></Link></li>
+                                        <li><Link to="/EnglishPL">English Premier League</Link></li>
+                                        <li><Link to="/MajorSL">Major Soccer League</Link></li>
+                                        <li><Link to="/GermanBL">German Bundesliga</Link></li>
                                     </ul>
                                 </div>
                             </div>
